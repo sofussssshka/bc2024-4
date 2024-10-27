96a848b935227a1c915a4046f364f751f925b18f
sofussssshka bc2024-4 <mulyavkosofiya@gmail.com>
2024-10-27 23:23:44 +0200

    Виконала 2 та 3 частину лабки

--- server.js
+++ server.js
@@ -1,44 +1,115 @@
-const http = require('http');
-const { Command } = require('commander');
-const fs = require('fs');
-
-const program = new Command();
+const { program } = require("commander");
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const superagent = require("superagent");
 
 program
-  .requiredOption('-h, --host <host>', 'Server host address')
-  .requiredOption('-p, --port <port>', 'Server port')
-  .requiredOption('-c, --cache <cachePath>', 'Cache directory path');
-
-const args = process.argv.slice(2);
-
-if (!args.includes('-h') && !args.includes('--host') ||
-    !args.includes('-p') && !args.includes('--port') ||
-    !args.includes('-c') && !args.includes('--cache')) {
-  console.log('Параметр не задано!');
-  process.exit(1);
-}
+  .option("-h, --host <server address>", "server address")
+  .option("-p, --port <server port>", "server port number")
+  .option("-c, --cache <path>", "path to the directory with cached files");
 
 program.parse(process.argv);
+const options = program.opts();
+const host = options.host;
+const port = options.port;
+const cache = options.cache;
 
-const { host, port, cache } = program.opts();
-
-
-if (isNaN(port) || port <= 0 || port > 65535) {
-  console.log('Помилка: Порт має бути числом в діапазоні від 1 до 65535!');
+if (!host) {
+  console.error("Впишіть хост!");
   process.exit(1);
 }
-
-
-if (!fs.existsSync(cache)) {
-  console.log('Помилка: Кеш-каталог не існує!');
+if (!port) {
+  console.error("Впишіть порт!");
+  process.exit(1);
+}
+if (!cache) {
+  console.error("Вкажіть шлях до папки в якій буде зберігатися кеш");
   process.exit(1);
 }
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('sofushka`s server \n');
+  const url = req.url;
+  const filePath = path.join(cache, `${url}.jpeg`);
+
+  if (!fs.existsSync(cache)) {
+    fs.mkdirSync(cache);
+  }
+
+  if (req.method === "GET") {
+    if (url === "/favicon.ico") {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
+
+    if (fs.existsSync(filePath)) {
+      fs.promises.readFile(filePath).then((data) => {
+        res.setHeader("Content-Type", "image/jpeg");
+        res.writeHead(200);
+        res.end(data);
+      });
+    } else {
+      superagent
+        .get(`https://http.cat${url}`)
+        .then((response) => {
+          const data = response.body;
+
+          fs.promises.writeFile(filePath, data).then(() => {
+            res.setHeader("Content-Type", "image/jpeg");
+            res.writeHead(200);
+            res.end(data);
+            return;
+          });
+        })
+        .catch((err) => {
+          res.writeHead(404);
+          res.end("Status Not Found");
+        });
+    }
+  } else if (req.method === "PUT") {
+    let body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const buffer = Buffer.concat(body);
+      fs.writeFile(filePath, buffer, (err) => {
+        if (!err) {
+          res.writeHead(201);
+          res.end("File created successfully");
+          return;
+        }
+        res.writeHead(500);
+        res.end("Server error");
+      });
+    });
+  } else if (req.method === "DELETE") {
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        if (!err) {
+          res.writeHead(200);
+          res.end("File deleted successfully");
+          return;
+        }
+        res.writeHead(500);
+        res.end("Server error");
+        return;
+      });
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.writeHead(404);
+      res.end("404 Not found");
+      return;
+    }
+  } else {
+    res.writeHead(405);
+    res.end("Method not allowed");
+  }
 });
 
 server.listen(port, host, () => {
-  console.log(`Сервер запущено  http://${host}:${port}`);
+  console.log(`Сервер запущений на http://${host}:${port}`);
 });
